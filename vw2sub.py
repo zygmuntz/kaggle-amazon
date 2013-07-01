ba13b39dc0095b70a8bdabdfd58ec5a0b9bb068c
zygmuntz <zajac.zygmunt@gmail.com>
2013-07-01 20:22:43 +0200

    removed tanh()

--- vw2sub.py
+++ vw2sub.py
@@ -1,6 +1,6 @@
 'convert VWs predictions into format suitable for submission'
 
-import sys, csv, math
+import sys, csv
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
@@ -12,8 +12,7 @@ writer.writerow( [ 'id', 'ACTION' ] )
 
 i = 1
 for line in reader:
-	raw_p = float( line[0] )
-	p = math.tanh( raw_p )
+	p = float( line[0] )
 	
 	writer.writerow( [ i, p ] )
 	i += 1
